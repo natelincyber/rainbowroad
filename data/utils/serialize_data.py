6f2f4f711c8be2df4f2deaa6d09e2d10e1c12071
natelincyber <natelin200510@gmail.com>
2024-05-28 20:41:56 -0700

    finished serialization and dataloader

--- data/utils/serialize_data.py
+++ data/utils/serialize_data.py
@@ -1,19 +1,34 @@
 # Simple data serialization for fast data loading during model training
 # Takes in images and projects and converts into a tflite file for use with a DataLoader
 
+from torchvision.transforms import v2
 import tensorflow as tf
 from PIL import Image
 import os
+import torch
+
+transforms = v2.Compose([
+    v2.PILToTensor(),
+    v2.Resize(size=(224, 224), antialias=True),
+    v2.ToDtype(torch.float32),
+    v2.Normalize(mean=[0.43625, 0.41656, 0.43584], std=[0.24982, 0.24742, 0.26399])
+])
 
 def _toBytes(image):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.io.encode_jpeg(image).numpy()]))
 
-def _toFloat64(*values):
+def _toFloat(*values):
     return tf.train.Feature(float_list=tf.train.FloatList(value=[*values]))
 
-# 
+
 def serialize(imagePath, labelPath):
     image = Image.open(imagePath)
+
+    base_width = 224
+    wpercent = (base_width / float(image.size[0]))
+    hsize = int((float(image.size[1]) * float(wpercent)))
+    image = image.resize((base_width, hsize), Image.Resampling.LANCZOS)
+
     feature = {
         'image': _toBytes(image)
     }
@@ -21,7 +36,7 @@ def serialize(imagePath, labelPath):
     with open(labelPath, 'r') as labels:
         for line in labels.readlines():
             label = line.strip().split()
-            feature[label[0]] = _toFloat64(float(label[1]), float(label[2]))
+            feature[label[0]] = _toFloat(float(label[1]), float(label[2]))
 
     proto = tf.train.Example(features=tf.train.Features(feature=feature))
 
@@ -32,14 +47,29 @@ def main():
     projections_data = os.path.join(dataset_path, "annotated_frames")
     projections_labels = os.path.join(dataset_path, "projections_2d")
     writer = tf.io.TFRecordWriter('dataset.tfrecords')
+    # mean = 0.0
+    # std = 0.0
+    # nb_samples = 0
+
 
     for i in os.listdir(projections_labels): # data_0, data_1, etc
         print("Reading:", i)
         data_jpg = [f for f in os.listdir(os.path.join(projections_data, i)) if f.lower().endswith(".jpg")]
         for img in data_jpg:
             tok1, _, tok2 = img.split('_')
+            # img = Image.open(os.path.join(projections_data,i, img))
+            # img = transforms(img)
+            # img = img.view(1, 3, -1)
+            # mean += img.mean(2).sum(0)
+            # std += img.std(2).sum(0)
+            # nb_samples += 1
             writer.write(serialize(os.path.join(projections_data, i, img), os.path.join(projections_labels, i, tok1+"_jointsCam_"+tok2.split('.')[0]+".txt")))
         print("serialized:", i)
+
+    # mean /= nb_samples
+    # std /= nb_samples
+    # print("Mean:", mean)
+    # print("Standard Deviation:", std)
     writer.close()
             
 
--- ml/dataloader.py
+++ ml/dataloader.py
@@ -1,6 +1,64 @@
-from torchvision.io import read_image
 from torch.utils.data import Dataset
+import tensorflow as tf
 
 class HandPose(Dataset):
-    def __init__(self, transform=None):
+    def __init__(self, filename="dataset.tfrecords", transform=None):
         self.transform = transform
+        self.feature_description = {
+            'image': tf.io.FixedLenSequenceFeature([], tf.string, allow_missing=True),
+            'F4_KNU1_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F4_KNU1_B': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F4_KNU2_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F4_KNU3_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F4_KNU3_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F3_KNU1_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F3_KNU1_B': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F3_KNU2_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F3_KNU3_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F1_KNU1_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F1_KNU1_B': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F1_KNU2_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F1_KNU3_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F2_KNU1_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F2_KNU1_B': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F2_KNU2_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'F2_KNU3_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'TH_KNU1_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'TH_KNU1_B': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'TH_KNU2_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'TH_KNU3_A': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
+            'PALM_POSITION': tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True)
+        }
+
+        raw_dataset = tf.data.TFRecordDataset(filename)
+        self.dataset = raw_dataset.map(self._parse_dataset)
+        self.indexedData = []
+
+        self.prepare_dataset()
+        del raw_dataset # send to gc to save memory
+
+
+    def _parse_dataset(self, proto):
+        return tf.io.parse_single_example(proto, self.feature_description)
+
+    def prepare_dataset(self):
+        for record in self.dataset:
+            self.indexedData.append(record)
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        data = self.indexedData[index]
+        image = data['image'].numpy()
+        labels = []
+        for item in data:
+            if item != 'image':
+                labels.append(data[item].numpy())
+        return image, labels
+        
+
+
+if __name__ == '__main__':
+    ds = HandPose(filename="D:\\Projects\\RainbowRoad\\data\\utils\\dataset.tfrecords")
+    print(ds[0])
